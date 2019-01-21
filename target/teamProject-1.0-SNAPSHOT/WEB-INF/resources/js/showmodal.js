e587684fd33bda1baf56c44f1cae62da9c43e60c
Mike G <gikasmike@gmail.com>
2019-01-21 21:53:59 +0200

    121

--- target/teamProject-1.0-SNAPSHOT/WEB-INF/resources/js/showmodal.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-// Open modal on page load
-$("#modal").modal('show');
-//redirect after 5seconds
-  setTimeout(function(){ 
-    $('#modal').modal('hide'); 
-     },1500);
-
-});
